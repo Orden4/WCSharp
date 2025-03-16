36318f1cd574e7ae580b86030e3cc9f065c87888
Orden4 <erxos479@gmail.com>
2025-03-16 21:53:41 +0100

    Testing with analyzers.

---
+++ WCSharp.Analyzer/WCSharp.Analyzer/WCSharpAnalyzerAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace WCSharp.Analyzer
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class WCSharpAnalyzerAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DIAGNOSTIC_ID = "WCSharpAnalyzer";
+		private const string CATEGORY = "Naming";
+
+		// You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+		// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+		private static LocalizableString Title { get; } = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+		private static LocalizableString MessageFormat { get; } = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+		private static LocalizableString Description { get; } = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+
+		private static DiagnosticDescriptor Rule { get; } = new DiagnosticDescriptor(
+			DIAGNOSTIC_ID,
+			Title,
+			MessageFormat,
+			CATEGORY,
+			DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			description: Description
+		);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+
+			// TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+			// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+			context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+		}
+
+		private static void AnalyzeSymbol(SymbolAnalysisContext context)
+		{
+			// TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+			var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+
+			// Find just those named type symbols with names containing lowercase letters.
+			if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+			{
+				// For all such symbols, produce a diagnostic.
+				var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+	}
+}
